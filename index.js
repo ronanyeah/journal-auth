bf271e21ad213a9f396fb1c35423af8b46240bc0
Ronan <ronan_mccabe@protonmail.com>
2019-01-04 03:06:11 +0000

    add spotify searching

--- index.js
+++ index.js
@@ -2,8 +2,13 @@ const { GraphQLClient } = require("graphql-request");
 const { promisify } = require("util");
 const { normalizeEmail, isEmail } = require("validator");
 const jwt = require("jsonwebtoken");
-const { GraphQLServer } = require("graphql-yoga");
-const { resolve } = require("path");
+const express = require("express");
+const { readFileSync } = require("fs");
+const expressGraphql = require("express-graphql");
+const { makeExecutableSchema } = require("graphql-tools");
+const axios = require("axios");
+
+let accessToken = "updog";
 
 const sign = promisify(jwt.sign);
 
@@ -11,8 +16,53 @@ const {
   HASURA_GRAPHQL_ENDPOINT,
   HASURA_GRAPHQL_ACCESS_KEY,
   JWT_SECRET,
-  PORT
+  PORT,
+  SPOTIFY_CLIENT_ID,
+  SPOTIFY_CLIENT_SECRET
 } = process.env;
+
+const typeDefs = readFileSync("./schema.graphql", "utf8");
+
+const authHeader =
+  "Basic " +
+  Buffer.from(`${SPOTIFY_CLIENT_ID}:${SPOTIFY_CLIENT_SECRET}`).toString(
+    "base64"
+  );
+
+const refreshToken = () =>
+  axios
+    .post("https://accounts.spotify.com/api/token", null, {
+      headers: {
+        Authorization: authHeader
+      },
+      params: { grant_type: "client_credentials" }
+    })
+    .then(({ data }) => (accessToken = data.access_token));
+
+const search = (query, i = 0) =>
+  axios
+    .get(`https://api.spotify.com/v1/search?q=${query}&type=track`, {
+      headers: {
+        Authorization: "Bearer " + accessToken
+      }
+    })
+    .then(({ data }) =>
+      data.tracks.items.map(song => ({
+        name: song.name,
+        id: song.id,
+        imageUrl: song.album.images[2].url,
+        artistName: song.artists[0].name
+      }))
+    )
+    .catch(async err => {
+      if (err.response.status === 401) {
+        await refreshToken();
+        return i >= 5 ? [] : search(query, i + 1);
+      } else {
+        console.log(err.response.status, err.response.statusText);
+        return [];
+      }
+    });
 
 const client = new GraphQLClient(
   HASURA_GRAPHQL_ENDPOINT + "/v1alpha1/graphql",
@@ -63,49 +113,61 @@ const token = id =>
     JWT_SECRET
   );
 
-new GraphQLServer({
-  port: PORT,
-  typeDefs: resolve(__dirname, "./schema.graphql"),
-  resolvers: {
-    Query: {
-      nonce: async (_, { email }) => {
-        const {
-          user: [u]
-        } = await client.request(getNonce(normalizeEmail(email)));
+const resolvers = {
+  Query: {
+    nonce: async (_, { email }) => {
+      const {
+        user: [u]
+      } = await client.request(getNonce(normalizeEmail(email)));
 
-        return u ? u.nonce : Error("invalid email or password");
-      }
+      return u ? u.nonce : Error("invalid email or password");
     },
-    Mutation: {
-      login: async (_, { email, password }) => {
-        const {
-          user: [u]
-        } = await client.request(query(normalizeEmail(email), password));
+    spotify: async (_, { query }) => search(query)
+  },
+  Mutation: {
+    login: async (_, { email, password }) => {
+      const {
+        user: [u]
+      } = await client.request(query(normalizeEmail(email), password));
 
-        return u ? token(u.id) : Error("invalid email or password");
-      },
-      signup: async (_, { email, password, nonce }) => {
-        if (!isEmail(email)) {
-          return Error("not a valid email address");
-        }
-        const data = await client
-          .request(insert(normalizeEmail(email), password, nonce))
-          .catch(() => null);
-
-        if (!data) {
-          return Error("this user already exists");
-        }
-
-        const {
-          insert_user: {
-            returning: [{ id }]
-          }
-        } = data;
-
-        return token(id);
+      return u ? token(u.id) : Error("invalid email or password");
+    },
+    signup: async (_, { email, password, nonce }) => {
+      if (!isEmail(email)) {
+        return Error("not a valid email address");
       }
+      const data = await client
+        .request(insert(normalizeEmail(email), password, nonce))
+        .catch(() => null);
+
+      if (!data) {
+        return Error("this user already exists");
+      }
+
+      const {
+        insert_user: {
+          returning: [{ id }]
+        }
+      } = data;
+
+      return token(id);
     }
   }
-}).start(({ port }) =>
-  console.log(`GraphQL server is running on port ${port}!`)
+};
+
+const app = express();
+
+app.use(
+  "/graphql",
+  expressGraphql({
+    schema: makeExecutableSchema({
+      typeDefs,
+      resolvers
+    }),
+    graphiql: true
+  })
+);
+
+app.listen(PORT, () =>
+  console.log(`GraphQL server is running on port ${PORT}!`)
 );
